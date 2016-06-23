9cfb51508cafe627c697dc14108faceef43a0d88
yangxiaohe <yangxiaohe@xiaomi.com>
2016-06-23 21:02:31 +0800

    update

--- +tools/android-mode/packages.el
+++ +tools/android-mode/packages.el
@@ -4,6 +4,7 @@
         (android-mode :location (recipe
                               :fetcher github
                               :repo "linux-xhyang/android-mode"))
+        elogcat
         ))
 
 (defun android-mode/init-android-mode ()
@@ -12,6 +13,16 @@
     :defer t
     :init
     (progn
-      (setq android-mode-sdk-dir "~/android-sdk/")
+      (setq android-mode-sdk-dir "")
+      (setq android-mode-sdk-tool-subdirs (mapcar #'append (split-string (concat (getenv "ANDROID_BUILD_PATHS") ":" (getenv "ANDROID_DEV_SCRIPTS") )":" t)))
       (require 'android-mode)
+      )))
+
+(defun android-mode/init-elogcat ()
+  "Initialize my package"
+  (use-package elogcat
+    :defer t
+    :init
+    (progn
+      (require 'elogcat)
       )))
