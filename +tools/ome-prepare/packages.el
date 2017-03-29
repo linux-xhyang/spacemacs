5314dadf000d6dc650bc6ecfd0b4a3c4e04d5fc0
yangxiaohe <yangxiaohe@xiaomi.com>
2017-03-29 18:08:29 +0800

    fix dependcy issue

--- +tools/ome-prepare/packages.el
+++ +tools/ome-prepare/packages.el
@@ -4,6 +4,20 @@
         url
         ))
 
+(defun run-su-shell (command)
+  (shell-command (concat "echo " (shell-quote-argument (read-passwd "Password? "))
+                         " | sudo -S " command))
+  )
+(defun run-shell (command)
+  (shell-command (concat command " " (shell-quote-argument (read-string "argument:"))))
+  )
+(defun which-utils(file)
+  (let ((path (split-string (getenv "PATH") ":")))
+    (cl-loop for dir in path
+             when (and (string-or-null-p dir) (file-exists-p dir) (file-exists-p (concat dir  "/" file)))
+             return (concat dir "/" file)
+             )))
+
 (defun ome-prepare/init-url ()
   "Initialize my package"
   (use-package url
