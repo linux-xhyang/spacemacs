69e84c362947aabe20f21ccd84e7497332933220
xhyang <linux.xhyang@gmail.com>
2017-03-30 22:30:49 +0800

    update

--- +tools/android-mode/packages.el
+++ +tools/android-mode/packages.el
@@ -7,16 +7,30 @@
         elogcat
         ))
 
+(defun android-mode/load-android-ide ()
+  (when (getenv "ANDROID_BUILD_TOP")
+    (let ((android-top (getenv "ANDROID_BUILD_TOP")))
+      (require 'android-mode)
+      (add-to-list 'load-path (concat android-top "/development/ide/emacs/"))
+      (add-to-list 'load-path (concat android-top "/prebuilts/devtools/tools/lib/"))
+      (require 'android)
+      (require 'android-compile)
+      (require 'android-host)
+      (unless (file-exists-p (concat (getenv "ANDROID_BUILD_TOP") "/.projectile"))
+        (write-region "" nil (concat (getenv "ANDROID_BUILD_TOP") "/.projectile")))
+      (setq android-build-top (getenv "ANDROID_BUILD_TOP"))
+      (setq android-mode-sdk-dir (concat (getenv "ANDROID_BUILD_TOP") "/prebuilts/devtools/"))
+      (setq android-mode-sdk-tool-subdirs (mapcar #'append (split-string (concat (getenv "ANDROID_BUILD_PATHS") ":" (getenv "ANDROID_DEV_SCRIPTS")
+                                                                                 ":" (getenv "PATH"))":" t)))
+      )))
+
 (defun android-mode/init-android-mode ()
   "Initialize my package"
   (use-package android-mode
     :defer t
     :init
     (progn
-      (setq android-mode-sdk-dir (concat (getenv "ANDROID_BUILD_TOP") "/prebuilts/devtools/"))
-      (setq android-mode-sdk-tool-subdirs (mapcar #'append (split-string (concat (getenv "ANDROID_BUILD_PATHS") ":" (getenv "ANDROID_DEV_SCRIPTS")
-                                                                                 ":" (getenv "PATH"))":" t)))
-      (require 'android-mode)
+      (android-mode/load-android-ide)
       )))
 
 (defun android-mode/init-elogcat ()
