b46c41d2f1ec9fe4883dc2241e595dc9df2d8084
yangxiaohe <yangxiaohe@xiaomi.com>
2017-05-04 18:52:38 +0800

    update eclipse check

--- +source-tools/ome-java/packages.el
+++ +source-tools/ome-java/packages.el
@@ -19,12 +19,14 @@
 (defun ome-java/init-eclim ()
   (use-package eclim
     :config
-    (require 'eclim)
-    (setq eclimd-autostart t)
-    (global-eclim-mode)
-    (require 'eclimd)
-    (ome/eclim-eclipse-directory)
-    (setq company-backends-java-mode '(company-emacs-eclim
-     (company-dabbrev-code company-keywords)
-     company-files company-dabbrev))
+    (when (executable-find "eclipse")
+      (require 'eclim)
+      (setq eclimd-autostart t)
+      (global-eclim-mode)
+      (require 'eclimd)
+      (ome/eclim-eclipse-directory)
+      (setq company-backends-java-mode '(company-emacs-eclim
+                                         (company-dabbrev-code company-keywords)
+                                         company-files company-dabbrev))
+      )
     ))
