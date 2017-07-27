b5fccd60722303262e83c4a13c5550f5570ad99f
yangxiaohe <yangxiaohe@xiaomi.com>
2017-07-27 16:36:51 +0800

    when java version not match then try jdk8

--- +source-tools/ome-java/funcs.el
+++ +source-tools/ome-java/funcs.el
@@ -1,11 +1,44 @@
 
+(defun ome/eclim-eclipse-valid ()
+  (cl-loop for dir in eclim-eclipse-dirs
+           when (and (message dir) (string-or-null-p dir) (file-exists-p dir) (file-exists-p (concat dir "/eclipse")))
+               return dir
+               ))
+
+(defun ome/eclim-eclipse-directory ()
+  (setq eclim-eclipse-dirs (append eclim-eclipse-dirs (list (getenv "HOME"))))
+  (unless (ome/eclim-eclipse-valid)
+    (let ((eclipse-dir (expand-file-name (read-directory-name "Eclipse Directory: "))))
+      (custom-set-variables
+       '(eclim-eclipse-dirs (list eclipse-dir))
+       '(eclim-executable (concat eclipse-dir "/eclim"))
+       ))))
+
+(defun ome/ome-java-version ()
+  (with-temp-buffer
+    (apply #'process-file "java" nil t nil '("-version"))
+    (goto-char (point-min))
+    (let ((line  (split-string (buffer-string) "\n" t)))
+      (dolist (version line)
+        (when (string-match-p "^java version" version)
+          (let ((ver (substring version 12)))
+            (when (string-match "^ \\\"\\([0-9]+\\.[0-9]+\\.[0-9]+\\)" ver)
+                (progn
+                  (cl-return (match-string 1 ver))
+                  ))
+            ))))))
+
 (defun custom-meghanada--start-server-process (orig-fun &rest args)
   "TODO: FIX DOC ."
   (let ((jar (meghanada--locate-server-jar)))
   (if (file-exists-p jar)
       (let ((process-connection-type nil)
             (process-adaptive-read-buffering nil)
-            (cmd (format "/usr/local/jdk8/bin/java %s %s -Dfile.encoding=UTF-8 -jar %s -p %d %s"
+            (cmd (format "%s %s %s -Dfile.encoding=UTF-8 -jar %s -p %d %s"
+                         (if (version<= (ome/ome-java-version) "1.8.0" )
+                             "/usr/local/jdk8/bin/java"
+                             "java"
+                             )
                          (meghanada--server-options)
                          meghanada-server-jvm-option
                          (shell-quote-argument jar)
@@ -29,18 +62,3 @@
     (message "%s"
              (substitute-command-keys
               "Missing server module. Type `\\[meghanada-install-server]' to install meghanada-server")))))
-
-(defun ome/eclim-eclipse-valid ()
-  (cl-loop for dir in eclim-eclipse-dirs
-           when (and (message dir) (string-or-null-p dir) (file-exists-p dir) (file-exists-p (concat dir "/eclipse")))
-               return dir
-               ))
-
-(defun ome/eclim-eclipse-directory ()
-  (setq eclim-eclipse-dirs (append eclim-eclipse-dirs (list (getenv "HOME"))))
-  (unless (ome/eclim-eclipse-valid)
-    (let ((eclipse-dir (expand-file-name (read-directory-name "Eclipse Directory: "))))
-      (custom-set-variables
-       '(eclim-eclipse-dirs (list eclipse-dir))
-       '(eclim-executable (concat eclipse-dir "/eclim"))
-       ))))
