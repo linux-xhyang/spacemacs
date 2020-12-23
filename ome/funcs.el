e19bf782238bd00afe312bd394dda0d6b4dcd808
yangxiaohe <linux.xhyang@gmail.com>
2020-12-23 11:40:46 +0800

    config smartparens keybindings
    Signed-off-by: yangxiaohe <linux.xhyang@gmail.com>

--- ome/funcs.el
+++ ome/funcs.el
@@ -57,4 +57,25 @@ candidates will be from company-tabnine, others keeping their own origin order."
         (setenv var value))
       (setq lst (cdr lst)))))
 
+(defmacro def-pairs (pairs)
+  "Define functions for pairing. PAIRS is an alist of (NAME . STRING)
+conses, where NAME is the function name that will be created and
+STRING is a single-character string that marks the opening character.
+
+  (def-pairs ((paren . \"(\")
+              (bracket . \"[\"))
+
+defines the functions WRAP-WITH-PAREN and WRAP-WITH-BRACKET,
+respectively."
+  `(progn
+     ,@(cl-loop for (key . val) in pairs
+             collect
+             `(defun ,(read (concat
+                             "wrap-with-"
+                             (prin1-to-string key)
+                             "s"))
+                  (&optional arg)
+                (interactive "p")
+                (sp-wrap-with-pair ,val)))))
+
 ;;; funcs.el ends here
