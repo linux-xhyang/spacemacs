ad2b53026eb2637204bba980adc9765b94f13206
xhyang <yangxiaohe@formovie.cn>
2021-06-25 16:00:11 +0800

    remove unused package

--- +tools/ome-gui/packages.el
+++ +tools/ome-gui/packages.el
@@ -1,8 +1,8 @@
-
 (setq ome-gui-packages
       '(
         ov
-        (rime :location (recipe :fetcher github :repo "DogLooksGood/emacs-rime"
+        (rime :toggle (display-graphic-p)
+         :location (recipe :fetcher github :repo "DogLooksGood/emacs-rime"
                                 :files ("Makefile" "lib.c" "rime*.el")))
         (clipetty :location (recipe :fetcher github :repo "spudlyo/clipetty"))
         cal-china-x
@@ -37,21 +37,24 @@
   "docstring"
   (use-package clipetty
     :ensure t
-    :hook (after-init . global-clipetty-mode))
+    :hook (after-init . global-clipetty-mode)
+    :config
+    (message "enable clipetty")
+    )
   )
 
 (defun org-latex-fragment-tooltip (beg end image imagetype)
   "Add the fragment tooltip to the overlay and set click function to toggle it."
   (overlay-put (ov-at) 'help-echo
-	           (concat (buffer-substring beg end)
-		               "\nmouse-1 to toggle."))
+               (concat (buffer-substring beg end)
+                       "\nmouse-1 to toggle."))
   (overlay-put (ov-at) 'local-map (let ((map (make-sparse-keymap)))
-				                    (define-key map (kbd "C-c C-x C-l") 'org-toggle-latex-fragment)
-				                    (define-key map [mouse-1]
-				                      `(lambda ()
-					                     (interactive)
-					                     (org-remove-latex-fragment-image-overlays ,beg ,end)))
-				                    map)))
+                                    (define-key map (kbd "C-c C-x C-l") 'org-toggle-latex-fragment)
+                                    (define-key map [mouse-1]
+                                      `(lambda ()
+                                         (interactive)
+                                         (org-remove-latex-fragment-image-overlays ,beg ,end)))
+                                    map)))
 
 (defun org-latex-fragment-justify (justification)
   "Justify the latex fragment at point with JUSTIFICATION.
@@ -62,30 +65,30 @@ JUSTIFICATION is a symbol for 'left, 'center or 'right."
                            nil t nil nil 'left))))
 
   (let* ((ov (ov-at))
-	     (beg (ov-beg ov))
-	     (end (ov-end ov))
-	     (shift (- beg (line-beginning-position)))
-	     (img (overlay-get ov 'display))
-	     (img (and (and img (consp img) (eq (car img) 'image)
-			            (image-type-available-p (plist-get (cdr img) :type)))
-		           img))
-	     space-left offset)
+         (beg (ov-beg ov))
+         (end (ov-end ov))
+         (shift (- beg (line-beginning-position)))
+         (img (overlay-get ov 'display))
+         (img (and (and img (consp img) (eq (car img) 'image)
+                        (image-type-available-p (plist-get (cdr img) :type)))
+                   img))
+         space-left offset)
     (when (and img
-	           ;; This means the equation is at the start of the line
-	           (= beg (line-beginning-position))
-	           (or
-		        (string= "" (s-trim (buffer-substring end (line-end-position))))
-		        (eq 'latex-environment (car (org-element-context)))))
+               ;; This means the equation is at the start of the line
+               (= beg (line-beginning-position))
+               (or
+                (string= "" (s-trim (buffer-substring end (line-end-position))))
+                (eq 'latex-environment (car (org-element-context)))))
       (setq space-left (- (window-max-chars-per-line) (car (image-size img)))
-	        offset (floor (cond
-			               ((eq justification 'center)
-			                (- (/ space-left 2) shift))
-			               ((eq justification 'right)
-			                (- space-left shift))
-			               (t
-			                0))))
+            offset (floor (cond
+                           ((eq justification 'center)
+                            (- (/ space-left 2) shift))
+                           ((eq justification 'right)
+                            (- space-left shift))
+                           (t
+                            0))))
       (when (>= offset 0)
-	    (overlay-put ov 'before-string (make-string offset ?\ ))))))
+        (overlay-put ov 'before-string (make-string offset ?\ ))))))
 
 (defun org-latex-fragment-justify-advice (beg end image imagetype)
   "After advice function to justify fragments."
