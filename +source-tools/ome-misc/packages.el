b696fdf50396627cb5f25e458f3bd271ad6ab63b
yangxiaohe <linux.xhyang@gmail.com>
2022-03-04 09:23:27 +0800

    add clerk-show keybindings

--- +source-tools/ome-misc/packages.el
+++ +source-tools/ome-misc/packages.el
@@ -74,60 +74,60 @@
     )
 
 (defun ome-misc/post-init-smartparens ()
-    "docstring"
-    (setq sp-ignore-modes-list (quote (minibuffer-inactive-mode
-                                          Info-mode
-                                          term-mode
-                                          org-mode
-                                          org-journal-mode
-                                          markdown-mode
-                                          ivy-occur-mode)))
-    (bind-keys
-        :map smartparens-mode-map
-        ("C-M-a" . sp-beginning-of-sexp)
-        ("C-M-e" . sp-end-of-sexp)
+  "docstring"
+  (setq sp-ignore-modes-list (quote (minibuffer-inactive-mode
+                                     Info-mode
+                                     term-mode
+                                     org-mode
+                                     org-journal-mode
+                                     markdown-mode
+                                     ivy-occur-mode)))
+  (bind-keys
+   :map smartparens-mode-map
+   ("C-M-a" . sp-beginning-of-sexp)
+   ("C-M-e" . sp-end-of-sexp)
 
-        ("C-<down>" . sp-down-sexp)
-        ("C-<up>"   . sp-up-sexp)
-        ("M-<down>" . sp-backward-down-sexp)
-        ("M-<up>"   . sp-backward-up-sexp)
+   ("C-<down>" . sp-down-sexp)
+   ("C-<up>"   . sp-up-sexp)
+   ("M-<down>" . sp-backward-down-sexp)
+   ("M-<up>"   . sp-backward-up-sexp)
 
-        ("C-M-f" . sp-forward-sexp)
-        ("C-M-b" . sp-backward-sexp)
+   ("C-M-f" . sp-forward-sexp)
+   ("C-M-b" . sp-backward-sexp)
 
-        ("C-M-n" . sp-next-sexp)
-        ("C-M-p" . sp-previous-sexp)
+   ("C-M-n" . sp-next-sexp)
+   ("C-M-p" . sp-previous-sexp)
 
-        ("C-S-f" . sp-forward-symbol)
-        ("C-S-b" . sp-backward-symbol)
+   ("C-S-f" . sp-forward-symbol)
+   ("C-S-b" . sp-backward-symbol)
 
-        ("C-<right>" . sp-forward-slurp-sexp)
-        ("M-<right>" . sp-forward-barf-sexp)
-        ("C-<left>"  . sp-backward-slurp-sexp)
-        ("M-<left>"  . sp-backward-barf-sexp)
+   ("C-<right>" . sp-slurp-hybrid-sexp)
+   ("M-<right>" . sp-forward-barf-sexp)
+   ("C-<left>"  . sp-backward-slurp-sexp)
+   ("M-<left>"  . sp-backward-barf-sexp)
 
-        ("C-M-t" . sp-transpose-sexp)
-        ("C-M-k" . sp-kill-sexp)
-        ("C-k"   . sp-kill-hybrid-sexp)
-        ("M-k"   . sp-backward-kill-sexp)
-        ("C-M-w" . sp-copy-sexp)
-        ("C-M-d" . kill-sexp)
+   ("C-M-t" . sp-transpose-sexp)
+   ("C-M-k" . sp-kill-sexp)
+   ("C-k"   . sp-kill-hybrid-sexp)
+   ("M-k"   . sp-backward-kill-sexp)
+   ("C-M-w" . sp-copy-sexp)
+   ("C-M-d" . kill-sexp)
 
-        ("M-<backspace>" . backward-kill-word)
-        ("C-<backspace>" . sp-backward-kill-word)
-        ([remap sp-backward-kill-word] . backward-kill-word)
+   ("M-<backspace>" . backward-kill-word)
+   ("C-<backspace>" . sp-backward-kill-word)
+   ([remap sp-backward-kill-word] . backward-kill-word)
 
-        ("C-M-u" . sp-backward-unwrap-sexp)
-        ("C-M-c" . sp-unwrap-sexp)
+   ("C-M-u" . sp-backward-unwrap-sexp)
+   ("C-M-c" . sp-unwrap-sexp)
 
-        ("C-x C-t" . sp-transpose-hybrid-sexp)
+   ("C-x C-t" . sp-transpose-hybrid-sexp)
 
-        ("C-c ("  . wrap-with-parens)
-        ("C-c ["  . wrap-with-brackets)
-        ("C-c {"  . wrap-with-braces)
-        ("C-c '"  . wrap-with-single-quotes)
-        ("C-c \"" . wrap-with-double-quotes)
-        ("C-c _"  . wrap-with-underscores)
-        ("C-c `"  . wrap-with-back-quotes)
-        )
-    )
+   ("C-c ("  . wrap-with-parens)
+   ("C-c ["  . wrap-with-brackets)
+   ("C-c {"  . wrap-with-braces)
+   ("C-c '"  . wrap-with-single-quotes)
+   ("C-c \"" . wrap-with-double-quotes)
+   ("C-c _"  . wrap-with-underscores)
+   ("C-c `"  . wrap-with-back-quotes)
+   )
+  )
--- +tools/ome-gui/keybindings.el
+++ +tools/ome-gui/keybindings.el
@@ -1,1 +1,13 @@
 (global-set-key [double-mouse-1] 'select-word)
+
+(defun clerk-show ()
+    (interactive)
+    (save-buffer)
+    (let
+        ((filename
+             (buffer-file-name)))
+        (when filename
+            (cider-interactive-eval
+                (concat "(nextjournal.clerk/show! \"" filename "\")")))))
+
+(global-set-key [f9] 'clerk-show)
